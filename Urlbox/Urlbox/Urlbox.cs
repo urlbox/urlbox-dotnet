0d8053969dbb3e90ac471d71b4d1994ec97067eb
Arnold Cubici-Jones <arnoldcubicijones@hotmail.co.uk>
2024-10-15 15:27:21 +0100

    feat(repo): add docbblocks to methods

--- Urlbox/Urlbox/Urlbox.cs
+++ Urlbox/Urlbox/Urlbox.cs
@@ -7,9 +7,14 @@ using System.Linq;
 using System.Collections.Generic;
 using System.Text;
 
-
 namespace Screenshots
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Urlbox"/> class with the provided API key and secret.
+    /// </summary>
+    /// <param name="key">Your Urlbox.com API Key.</param>
+    /// <param name="secret">Your Urlbox.com API Secret.</param>
+    /// <exception cref="ArgumentException">Thrown when the API key or secret is invalid.</exception>
     public class Urlbox
     {
         private String key;
@@ -31,11 +36,23 @@ namespace Screenshots
             this.urlGenerator = new UrlGenerator(key, secret);
         }
 
-        public async Task<string> DownloadAsBase64(IDictionary<string, object> options, string format = "png"){
+        /// <summary>
+        /// Downloads a screenshot as a Base64-encoded string from a Urlbox render link.
+        /// </summary>
+        /// <param name="options">The options for the screenshot</param>
+        /// <param name="format">The image format (e.g., "png", "jpg").</param>
+        /// <returns>A Base64-encoded string of the screenshot.</returns>
+        public async Task<string> DownloadAsBase64(IDictionary<string, object> options, string format = "png")
+        {
             var urlboxUrl = this.GenerateUrlboxUrl(options, format);
             return await DownloadAsBase64(urlboxUrl);
         }
 
+        /// <summary>
+        /// Downloads a screenshot as a Base64-encoded string from the given Urlbox URL.
+        /// </summary>
+        /// <param name="urlboxUrl">The render link Urlbox URL.</param>
+        /// <returns>A Base64-encoded string of the screenshot.</returns>
         public async Task<string> DownloadAsBase64(string urlboxUrl)
         {
             Func<HttpResponseMessage, Task<string>> onSuccess = async (result) =>
@@ -48,11 +65,25 @@ namespace Screenshots
             return await this.Download(urlboxUrl, onSuccess);
         }
 
-        public async Task<string> DownloadToFile(IDictionary<string, object> options, string filename, string format = "png"){
+        /// <summary>
+        /// Downloads a screenshot and saves it as a file.
+        /// </summary>
+        /// <param name="options">The options for the screenshot.</param>
+        /// <param name="filename">The file path where the screenshot will be saved.</param>
+        /// <param name="format">The image format (e.g., "png", "jpg"). Default is "png".</param>
+        /// <returns>The contents of the downloaded file as a string.</returns>
+        public async Task<string> DownloadToFile(IDictionary<string, object> options, string filename, string format = "png")
+        {
             var urlboxUrl = GenerateUrlboxUrl(options, format);
-            return await DownloadToFile(urlboxUrl, filename);    
+            return await DownloadToFile(urlboxUrl, filename);
         }
 
+        /// <summary>
+        /// Downloads a screenshot from the given Urlbox URL and saves it as a file.
+        /// </summary>
+        /// <param name="urlboxUrl">The render link Urlbox URL.</param>
+        /// <param name="filename">The file path where the screenshot will be saved.</param>
+        /// <returns>The contents of the downloaded file.</returns>
         public async Task<string> DownloadToFile(string urlboxUrl, string filename)
         {
             Func<HttpResponseMessage, Task<string>> onSuccess = async (result) =>
@@ -68,8 +99,14 @@ namespace Screenshots
             return await Download(urlboxUrl, onSuccess);
         }
 
+        /// <summary>
+        /// Downloads content from the given Urlbox render link and processes it using the provided onSuccess function.
+        /// </summary>
+        /// <param name="urlboxUrl">The render link Urlbox URL.</param>
+        /// <param name="onSuccess">The function to execute when the download is successful.</param>
+        /// <returns>The result of the success function.</returns>
         private async Task<string> Download(string urlboxUrl, Func<HttpResponseMessage, Task<string>> onSuccess)
-        {            
+        {
             using (var client = new HttpClient())
             {
                 using (var result = await client.GetAsync(urlboxUrl).ConfigureAwait(false))
@@ -88,21 +125,42 @@ namespace Screenshots
             }
         }
 
+        /// <summary>
+        /// Generates a URL for a PNG screenshot using the provided options.
+        /// </summary>
+        /// <param name="options">The options for the screenshot.</param>
+        /// <returns>A render link Url to render a PNG screenshot.</returns>
         public string GeneratePNGUrl(IDictionary<string, object> options)
         {
             return GenerateUrlboxUrl(options, "png");
         }
 
+        /// <summary>
+        /// Generates a URL for a JPEG screenshot using the provided options.
+        /// </summary>
+        /// <param name="options">The options for the screenshot.</param>
+        /// <returns>A render link Url to render a JPEG screenshot.</returns>
         public string GenerateJPEGUrl(IDictionary<string, object> options)
         {
             return GenerateUrlboxUrl(options, "jpg");
         }
 
+        /// <summary>
+        /// Generates a URL for a PDF file using the provided options.
+        /// </summary>
+        /// <param name="options">The options for generating the PDF.</param>
+        /// <returns>A render link Url to render a PDF file.</returns>
         public string GeneratePDFUrl(IDictionary<string, object> options)
         {
             return GenerateUrlboxUrl(options, "pdf");
         }
 
+        /// <summary>
+        /// Generates a Urlbox URL with the specified format.
+        /// </summary>
+        /// <param name="options">The options for generating the screenshot or PDF.</param>
+        /// <param name="format">The format of the output, e.g., "png", "jpg", "pdf".</param>
+        /// <returns>A render link URL to render the content.</returns>
         public string GenerateUrlboxUrl(IDictionary<string, object> options, string format = "png")
         {
             return urlGenerator.GenerateUrlboxUrl(options, format);
